5b9bacadc0ebc5bcde536de68d7a33ce90d71d0b
Guy Marshall <guymarshall9@gmail.com>
2022-08-20 22:44:12 +0100

    Finish converting code - time for testing

--- main.f95
+++ main.f95
@@ -11,8 +11,8 @@ integer :: delta_t = 1
 integer :: time = 0;
 real(8) :: acceleration;
 real(8) :: velocity = 0.0;
-real(8) :: deltaV;
-real(8) :: deltaS;
+real(8) :: delta_v;
+real(8) :: delta_s;
 
 print *, "Choose your planet!"
 print *, "1 - Mercury"
@@ -48,23 +48,23 @@ select case (choice)
     case (9)
         radius = 5900000000000.0
     case default
-        call exit(0)
+        call exit(1)
 end select
 
+do
+    acceleration = -1.0 * (GRAVITATIONAL_CONSTANT * MASS_OF_SUN_KG) / (radius ** 2)
+    delta_v = acceleration * delta_t
+    velocity = velocity + delta_v
+    delta_s = velocity * delta_t
+    radius = radius + delta_s
+
+    if (radius <= 0.0)
+        call exit(0)
+    end if
+
+    print *, "Time: ", time, "s, acceleration: ", acceleration, "ms^-2, velocity: ", velocity, "ms^-1, distance: ", radius, "m"
+
+    time = time + delta_t
+end do
+
 end program main
-
-! while (true) {
-!     acceleration = -1 * (GRAVITATIONAL_CONSTANT * MASS_OF_SUN_KG) / (radius * radius);
-!     deltaV = acceleration * deltaT;
-!     velocity += deltaV;
-!     deltaS = velocity * deltaT;
-!     radius += deltaS;
-
-!     if (radius <= 0.0) {
-!         break;
-!     }
-
-!     System.out.printf("Time: %ds, acceleration: %fms^-2, velocity: %fms^-1, distance: %fm%n", time, acceleration, velocity, radius);
-
-!     time += deltaT;
-! }
